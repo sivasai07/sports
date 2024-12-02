086dcec60d12e534a53a864e91b99435303802b9
Gokul Thakral <124121021+GokulT19@users.noreply.github.com>
2024-12-02 15:20:51 +0530

    Delete Authentication-main directory

--- Authentication-main/src/main/java/com/auth/Authentication/Repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package com.auth.Authentication.Repository;
-
-import com.auth.Authentication.entity.Coach;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CoachRepository extends JpaRepository<Coach, Integer> {
-    Coach findByUsername(String username); // Custom method to find by username
-}
